da175bfbf5a117014f5315fe21d673c314ae22ab
Kanya-Mo <kanya.mo@intel.com>
2025-03-19 14:52:12 -0700

    Enable xccl in more recipes

--- recipes/qat_distributed.py
+++ recipes/qat_distributed.py
@@ -947,7 +947,12 @@ def recipe_main(cfg: DictConfig) -> None:
             "Distributed finetune recipe should be run via a distributed launcher."
             "If using tune CLI, please specify --nnodes 1 and --nproc_per_node [num_gpus]"
         )
-    init_process_group("cuda:nccl,cpu:gloo")
+    distributed_backend = training.get_distributed_backend(
+        cfg.get("device", "cpu"),
+        offload_ops_to_cpu=cfg.get("fsdp_cpu_offload", False)
+                           or cfg.get("enable_async_checkpointing", False),
+    )
+    init_process_group(distributed_backend)
     if cfg.get("fsdp_cpu_offload", False):
         # Utilize all available CPU cores for intra-op parallelism. This provides ~2x
         # speed up when benchmarking fused AdamW on CPU
